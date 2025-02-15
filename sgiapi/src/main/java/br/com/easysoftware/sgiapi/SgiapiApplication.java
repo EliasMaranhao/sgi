e667f97fb7220edfd2c1a68db3e08e9ed0d00d52
EliasMaranhao <elias.ma.antonio@gmail.com>
2025-02-14 22:34:08 -0300

    inicio do projeto

---
+++ sgiapi/src/main/java/br/com/easysoftware/sgiapi/SgiapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.easysoftware.sgiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgiapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgiapiApplication.class, args);
+	}
+
+}
---
+++ sgiapi/src/test/java/br/com/easysoftware/sgiapi/SgiapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.easysoftware.sgiapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgiapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
