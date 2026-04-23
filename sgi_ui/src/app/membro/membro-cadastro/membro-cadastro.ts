903a80c5a625872dffb5208b72d981d023536994
EliasMaranhao <elias.ma.antonio@gmail.com>
2026-04-23 07:23:19 -0300

    campo de foto

--- sgi_ui/src/app/membro/membro-cadastro/membro-cadastro.ts
+++ sgi_ui/src/app/membro/membro-cadastro/membro-cadastro.ts
@@ -31,9 +31,12 @@ import { MatButtonModule } from '@angular/material/button';
 })
 export class MembroCadastro {
   membroForm: FormGroup;
-  private dialog = inject(MatDialog); // Injeção moderna (inject)
+  imagePreview: string | null = null;
 
-  constructor(private fb: FormBuilder){
+  private dialog = inject(MatDialog); // Injeção moderna (inject)
+  private fb = inject(FormBuilder);
+
+  constructor(){
     this.membroForm = this.fb.group({
       nome: ['', [Validators.required, Validators.minLength(10)]],
       dataNascimento: ['', [Validators.required, Validators.minLength(8)]],
@@ -41,6 +44,7 @@ export class MembroCadastro {
       tipo: ['', [Validators.required]],
       email: ['', [Validators.required, Validators.email]],
       celular: [''],
+      foto: [null],
       endereco: this.fb.group({
         rua: ['', [Validators.required]],
         numero: ['', [Validators.required]],
@@ -73,5 +77,32 @@ export class MembroCadastro {
     dialogRef.afterClosed().subscribe(result => {
       console.log('O modal foi fechado. Resultado:', result);
     });
+  }
+
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+
+    if (file) {
+      // 1. Atualiza o valor no formulário (o arquivo em si para enviar ao backend)
+      // Se já existia um preview anterior, limpamos a memória antes de criar o novo
+      this.limparPreviewAnterior();
+      // Cria a URL de atalho
+      this.imagePreview = URL.createObjectURL(file);
+
+      // Atualiza seu formulário reativo com o arquivo original
+      this.membroForm.patchValue({ foto: file });
+    }
+  }
+
+  limparPreviewAnterior() {
+    if (this.imagePreview) {
+      URL.revokeObjectURL(this.imagePreview);
+      this.imagePreview = null;
+    }
+  }
+
+  // Boa prática: limpar a memória quando o usuário sai da tela
+  ngOnDestroy() {
+    this.limparPreviewAnterior();
   }
 }
